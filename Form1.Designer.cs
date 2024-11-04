2b26c57437a128518bca21978b201660fbc2b139
Sainan <sainan@calamity.inc>
2024-11-04 21:45:56 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace AmIBattlEyeBanned
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.bancheck_btn = new System.Windows.Forms.Button();
+            this.bancheck_status = new System.Windows.Forms.Label();
+            this.bancheck_input = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.name2rid_btn = new System.Windows.Forms.Button();
+            this.name2rid_status = new System.Windows.Forms.Label();
+            this.name2rid_input = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bancheck_btn);
+            this.groupBox1.Controls.Add(this.bancheck_status);
+            this.groupBox1.Controls.Add(this.bancheck_input);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(210, 70);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Check BattlEye Ban by Rockstar ID";
+            // 
+            // bancheck_btn
+            // 
+            this.bancheck_btn.Location = new System.Drawing.Point(155, 17);
+            this.bancheck_btn.Name = "bancheck_btn";
+            this.bancheck_btn.Size = new System.Drawing.Size(49, 23);
+            this.bancheck_btn.TabIndex = 2;
+            this.bancheck_btn.Text = "Submit";
+            this.bancheck_btn.UseVisualStyleBackColor = true;
+            this.bancheck_btn.Click += new System.EventHandler(this.bancheck_btn_Click);
+            // 
+            // bancheck_status
+            // 
+            this.bancheck_status.AutoSize = true;
+            this.bancheck_status.Location = new System.Drawing.Point(6, 46);
+            this.bancheck_status.Name = "bancheck_status";
+            this.bancheck_status.Size = new System.Drawing.Size(76, 13);
+            this.bancheck_status.TabIndex = 1;
+            this.bancheck_status.Text = "Awaiting input.";
+            // 
+            // bancheck_input
+            // 
+            this.bancheck_input.Location = new System.Drawing.Point(6, 19);
+            this.bancheck_input.Name = "bancheck_input";
+            this.bancheck_input.Size = new System.Drawing.Size(143, 20);
+            this.bancheck_input.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.linkLabel1);
+            this.groupBox2.Controls.Add(this.name2rid_btn);
+            this.groupBox2.Controls.Add(this.name2rid_status);
+            this.groupBox2.Controls.Add(this.name2rid_input);
+            this.groupBox2.Location = new System.Drawing.Point(228, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(210, 70);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Convert Username to Rockstar ID";
+            // 
+            // name2rid_btn
+            // 
+            this.name2rid_btn.Location = new System.Drawing.Point(155, 17);
+            this.name2rid_btn.Name = "name2rid_btn";
+            this.name2rid_btn.Size = new System.Drawing.Size(49, 23);
+            this.name2rid_btn.TabIndex = 3;
+            this.name2rid_btn.Text = "Submit";
+            this.name2rid_btn.UseVisualStyleBackColor = true;
+            this.name2rid_btn.Click += new System.EventHandler(this.name2rid_btn_Click);
+            // 
+            // name2rid_status
+            // 
+            this.name2rid_status.AutoSize = true;
+            this.name2rid_status.Location = new System.Drawing.Point(7, 46);
+            this.name2rid_status.Name = "name2rid_status";
+            this.name2rid_status.Size = new System.Drawing.Size(76, 13);
+            this.name2rid_status.TabIndex = 1;
+            this.name2rid_status.Text = "Awaiting input.";
+            // 
+            // name2rid_input
+            // 
+            this.name2rid_input.Location = new System.Drawing.Point(6, 19);
+            this.name2rid_input.Name = "name2rid_input";
+            this.name2rid_input.Size = new System.Drawing.Size(143, 20);
+            this.name2rid_input.TabIndex = 0;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(102, 46);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(102, 13);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Try the manual way.";
+            this.linkLabel1.Visible = false;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 94);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Am I BattlEye-banned?";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox bancheck_input;
+        private System.Windows.Forms.Label bancheck_status;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label name2rid_status;
+        private System.Windows.Forms.TextBox name2rid_input;
+        private System.Windows.Forms.Button bancheck_btn;
+        private System.Windows.Forms.Button name2rid_btn;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Diagnostics;
+using System.Net.Http;
+
+namespace AmIBattlEyeBanned
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string computeBeId(long rid)
+        {
+            using (var md5 = MD5.Create())
+            {
+                byte[] digest = md5.ComputeHash(Encoding.ASCII.GetBytes("BE" + Convert.ToBase64String(Encoding.UTF8.GetBytes(rid.ToString()))));
+                string hexDigest = "";
+                for (int i = 0; i != digest.Length; ++i)
+                {
+                    hexDigest += digest[i].ToString("x2").ToLower();
+                }
+                return hexDigest;
+            }
+        }
+
+        private string checkBanReason(long rid)
+        {
+            using (var client = new UdpClient())
+            {
+                // "Connect" to paradise1.battleye.com
+                IPEndPoint ep = new IPEndPoint(IPAddress.Parse("51.89.97.102"), 61455);
+                client.Connect(ep);
+
+                // Send
+                var outgoingData = new byte[4];
+                var rng = new Random();
+                for (int i = 0; i != outgoingData.Length; ++i)
+                {
+                    outgoingData[i] = (byte)rng.Next(0, 256);
+                }
+                outgoingData = outgoingData.Concat(Encoding.UTF8.GetBytes(computeBeId(rid))).ToArray();
+                client.Send(outgoingData, outgoingData.Length);
+
+                // Send
+                byte[] receivedData = client.Receive(ref ep);
+                return Encoding.ASCII.GetString(receivedData.Skip(4).ToArray());
+            }
+        }
+
+        private void bancheck_btn_Click(object sender, EventArgs e)
+        {
+            long rid;
+            if (long.TryParse(bancheck_input.Text, out rid))
+            {
+                var ban_reason = checkBanReason(rid);
+                if (ban_reason == "")
+                {
+                    bancheck_status.Text = "RID " + rid + " is not banned.";
+                }
+                else
+                {
+                    bancheck_status.Text = ban_reason;
+                }
+            }
+            else
+            {
+                bancheck_input.Text = "Invalid input.";
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://amibattleyebanned.com/resolve-rid-steps.png");
+        }
+
+        private async void name2rid_btn_Click(object sender, EventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string content = await client.GetStringAsync("https://sc-cache.com/n/" + name2rid_input.Text);
+                    if (content.Substring(0, 6) == "{\"id\":")
+                    {
+                        bancheck_input.Text = content.Substring(6, content.IndexOf(',') - 6);
+                        name2rid_status.Text = "Filled in RID!";
+                        linkLabel1.Visible = false;
+                        return;
+                    }
+                }
+                catch (Exception)
+                {
+                }
+                name2rid_status.Text = "Not in cache.";
+                linkLabel1.Visible = true;
+            }
+        }
+    }
+}
